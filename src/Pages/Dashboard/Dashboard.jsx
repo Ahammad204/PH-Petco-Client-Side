1667d370954d816aecb8abd8222b7627a1539e6a
Ahammad204 <Ahammadcode@gmail.com>
2023-11-29 13:57:31 +0600

    add icon for each dashboard route

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
-import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaSearch, FaShoppingCart, FaUsers, FaUtensils } from "react-icons/fa";
+import {  FaHome, FaUsers, } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../../Hooks/useAdmin";
+import { FaHandHoldingDollar, FaPaw } from "react-icons/fa6";
 
 
 
@@ -13,7 +14,7 @@ const Dashboard = () => {
     return (
         <div className="flex">
             {/* dashboard side bar */}
-            <div className="w-64 min-h-screen bg-orange-400">
+            <div className="w-64 min-h-screen bg-[#af4e45] text-white">
                 <ul className="menu p-4">
                     {
                         isAdmin ? <>
@@ -23,72 +24,71 @@ const Dashboard = () => {
                                     Admin Home</NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/addItems">
-                                    <FaUtensils></FaUtensils>
-                                    Add Items</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/manageItems">
-                                    <FaList></FaList>
-                                    Manage Items</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/bookings">
-                                    <FaBook></FaBook>
-                                    Manage Bookings</NavLink>
-                            </li>
-                            <li>
                                 <NavLink to="/dashboard/users">
                                     <FaUsers></FaUsers>
                                     All Users</NavLink>
                             </li>
+                            <li>
+                                <NavLink to="/dashboard/allPets">
+                                    <FaPaw></FaPaw>
+                                    All Pets</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/allDonation">
+                                    <FaHandHoldingDollar></FaHandHoldingDollar>
+                                    All Donation</NavLink>
+                            </li>
+                            <div className="divider"></div>
                         </>
                             :
                             <>
-                                <li>
-                                    <NavLink to="/dashboard/userHome">
-                                        <FaHome></FaHome>
-                                        User Home</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/history">
-                                        <FaCalendar></FaCalendar>
-                                        Not History</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/cart">
-                                        <FaShoppingCart></FaShoppingCart>
-                                        My Cart </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/review">
-                                        <FaAd></FaAd>
-                                        Add a Review</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/paymentHistory">
-                                        <FaList></FaList>
-                                        Real Payment History</NavLink>
-                                </li>
+
                             </>
                     }
                     {/* shared nav links */}
-                    <div className="divider"></div>
+
                     <li>
                         <NavLink to="/">
                             <FaHome></FaHome>
                             Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/order/salad">
-                            <FaSearch></FaSearch>
-                            Menu</NavLink>
+                        <NavLink to="/dashboard/userHome">
+                            <FaHome></FaHome>
+                            User Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/order/contact">
-                            <FaEnvelope></FaEnvelope>
-                            Contact</NavLink>
+                        <NavLink to="/dashboard/addPet">
+                            <FaPaw></FaPaw>
+                            Add A Pet</NavLink>
                     </li>
+                    <li>
+                        <NavLink to="/dashboard/addedPet">
+                        <FaPaw></FaPaw>
+                            My Added Pets </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/adoptionRequest">
+                        <FaHandHoldingDollar></FaHandHoldingDollar>
+                            Adoption Request</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/createDonationCampaign">
+                        <FaHandHoldingDollar></FaHandHoldingDollar>
+                            Create Donation Campaign</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/myDonationCampaign">
+                        <FaHandHoldingDollar></FaHandHoldingDollar>
+                            My Donation Campaign</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/myDonation">
+                        <FaHandHoldingDollar></FaHandHoldingDollar>
+                            My Donation </NavLink>
+                    </li>
+
+
                 </ul>
             </div>
             {/* dashboard content */}
